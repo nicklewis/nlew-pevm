ece86d6145a691992d6f7e681d34cccaa7fddcf1
Nick Lewis <nick@puppet.com>
2020-10-20 11:55:20 -0700

    Lookup master from inventory

---
+++ Puppetfile
@@ -0,0 +1,5 @@
+# This Puppetfile is managed by Bolt. Do not edit it directly.
+# Use `bolt module add` to install new modules.
+moduledir '.modules'
+mod "nlew-floaty", "0.1.2"
+mod "puppetlabs-bootstrap", "2.1.0"
--- plans/init.pp
+++ plans/init.pp
@@ -3,35 +3,25 @@
 #
 # @param version The version of Puppet Enterprise to install
 # @param password The console admin user password
-# @param master The hostname of the server to use as the PE master
 # @param agents How many agents, if any, to provision and connect
 plan pevm(
   String $version = 'latest',
-  String $password = 'istrator',
-  TargetSpec $master = undef,
+  String $password = lookup('console_password'),
   Integer $agents = 0,
 ) {
+  $master = get_target('master')
   $platform = 'centos-7-x86_64'
 
-  if $master {
-    $master_target = $master.get_target
-  } else {
-    $master_target = floaty::get($platform).get_target
-    run_command("floaty modify ${master_target} --tags '{\"pevm\": \"master\"}'", localhost)
-  }
+  run_task('pevm::install', $master, "install PE", password => $password)
 
-  run_task('pevm::install', $master_target, "install PE on the master", password => $password)
-
-  run_task(pevm::run_agent, $master_target, "run puppet on the master to finish the setup")
+  run_task(pevm::run_agent, $master, "run puppet on the master to finish the setup")
 
   # XXX This should be a task
-  run_command("echo ${password} | /opt/puppetlabs/bin/puppet-access login admin --lifetime 7d", $master_target, "create RBAC token")
+  run_command("echo ${password} | /opt/puppetlabs/bin/puppet-access login admin --lifetime 7d", $master, "create RBAC token")
 
   if $agents > 0 {
-    run_plan(pevm::addnodes, master => $master_target, platform => $platform, count => $agents)
+    run_plan(pevm::addnodes, master => $master, platform => $platform, count => $agents)
   }
 
   out::message("Finished installing Puppet Enterprise on a ${platform} host: ${master}")
-
-  return $master_target
 }
